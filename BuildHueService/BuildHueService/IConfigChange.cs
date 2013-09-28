f08b13c5cf630363cc51eaecbe4328929ebf116e
Dave Brotherstone <davegb@pobox.com>
2013-09-28 23:17:47 +0100

    IConfigChange interface defined

---
+++ BuildHueService/BuildHueService/IConfigChange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BuildHueService
+{
+    public interface IConfigChange
+    {
+        // /buildConfig/the_build_name/ff0000/1,2   
+        [WebInvoke(Method = "POST", UriTemplate = "buildConfig/{build}/{htmlColour}/{commaSeperatedLights}")]
+        void SetBuildConfig(string build, string htmlColour, string commaSeperatedLights);
+
+        [WebGet(UriTemplate = "buildConfig")]
+        BuildConfigurationsJson GetBuildConfigs();
+    }
+
+    public class BuildConfigurationsJson
+    {
+        public List<BuildConfigurationJson> BuildConfigurations { get; set; }
+    }
+
+    public class BuildConfigurationJson
+    {
+        public string Name { get; set; }
+
+        public string HtmlColour { get; set; }
+
+        public List<int> Lights { get; set; }
+    }
+}
